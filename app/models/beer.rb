74d50cc823e71d08f36a00ca51649763889dff92
Sara Kauvo <sara.kau@hotmail.com>
2016-01-25 22:56:54 +0200

    Rating tallennus

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,10 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    sum = self.ratings.map { |r| r.score }
+    sum.sum/sum.count.to_f
+  end
+
 end
