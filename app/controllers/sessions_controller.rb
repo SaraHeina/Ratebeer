d913fd84d2b907c459b29edf872a660d3e52b3c3
Sara Kauvo <sara.kau@hotmail.com>
2016-02-02 21:04:14 +0200

    club

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,11 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by username: params[:username]
-    if user.nil?
-      redirect_to :back, notice: "User #{params[:username]} does not exist!"
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user), notice: "Welcome back!"
     else
-    session[:user_id] = user.id if not user.nil?
-    redirect_to user
+      redirect_to :back, notice: "Username and/or password mismatch"
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,22 @@
 class User < ActiveRecord::Base
   has_many :ratings
+  has_many :memberships
   has_many :beers, through: :ratings
+  has_many :beer_clubs, through: :memberships
 
   validates :username, uniqueness: true
   validates :username, length: { minimum: 3 }
   validates :username, length: { maximum: 15 }
 
+  PASSWORD_FORMAT = /\A
+  (?=.*\d)           # Must contain a digit
+  (?=.*[A-Z])        # Must contain an upper case character
+/x
+
+  validates :password, length: { minimum: 4 }
+  validates :password, format: {with: PASSWORD_FORMAT, message: "must have at least 1 number and 1 big letter" }
+
   include RatingAverage
+
+  has_secure_password
 end
