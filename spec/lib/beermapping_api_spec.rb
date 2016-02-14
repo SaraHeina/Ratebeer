e9d4069f61312b28d0fc8df518e33425f50dd8f4
Sara Kauvo <sara.kau@hotmail.com>
2016-02-14 22:04:26 +0200

    Apikey

--- spec/lib/beermapping_api_spec.rb
+++ spec/lib/beermapping_api_spec.rb
@@ -1,73 +1,75 @@
 require 'rails_helper'
 
 describe "BeermappingApi" do
-  describe "in case of cache miss" do
+    describe "in case of cache miss" do
 
-    before :each do
-      Rails.cache.clear
-    end
+      before :each do
+        Rails.cache.clear
+      end
 
-  it "When HTTP GET returns one entry, it is parsed and returned" do
-
-    canned_answer = <<-END_OF_STRING
+      it "When HTTP GET returns one entry, it is parsed and returned" do
+        canned_answer = <<-END_OF_STRING
 <?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id>12411</id><name>Gallows Bird</name><status>Brewery</status><reviewlink>http://beermapping.com/maps/reviews/reviews.php?locid=12411</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=12411&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=12411&amp;d=1&amp;type=norm</blogmap><street>Merituulentie 30</street><city>Espoo</city><state></state><zip>02200</zip><country>Finland</country><phone>+358 9 412 3253</phone><overall>91.66665</overall><imagecount>0</imagecount></location></bmp_locations>
-    END_OF_STRING
+        END_OF_STRING
 
-    stub_request(:get, /.*espoo/).to_return(body: canned_answer, headers: { 'Content-Type' => "text/xml" })
+        stub_request(:get, /.*espoo/).to_return(body: canned_answer, headers: {'Content-Type' => "text/xml"})
 
-    places = BeermappingApi.places_in("espoo")
+        places = BeermappingApi.places_in("espoo")
 
-    expect(places.size).to eq(1)
-    place = places.first
-    expect(place.name).to eq("Gallows Bird")
-    expect(place.street).to eq("Merituulentie 30")
-  end
+        expect(places.size).to eq(1)
+        place = places.first
+        expect(place.name).to eq("Gallows Bird")
+        expect(place.street).to eq("Merituulentie 30")
+      end
 
-  it "When HTTP GET returns no entries, empty set is returned" do
-    canned_answer = <<-END_OF_STRING
-<?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id></id><name></name><status></status><reviewlink></reviewlink><proxylink></proxylink><blogmap></blogmap><street></street><city></city><state></state><zip></zip><country></country><phone></phone><overall></overall><imagecount></imagecount></location></bmp_locations>
-    END_OF_STRING
+      it "When HTTP GET returns no entries, empty set is returned" do
 
-    stub_request(:get, /.*kumpula/).to_return(body: canned_answer, headers: { 'Content-Type' => "text/xml" })
+        canned_answer = <<-END_OF_STRING
+  <?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id></id><name></name><status></status><reviewlink></reviewlink><proxylink></proxylink><blogmap></blogmap><street></street><city></city><state></state><zip></zip><country></country><phone></phone><overall></overall><imagecount></imagecount></location></bmp_locations>
+        END_OF_STRING
 
-    places = BeermappingApi.places_in("kumpula")
+        stub_request(:get, /.*kerava/).to_return(body: canned_answer, headers: { 'Content-Type' => "text/xml" })
 
-    expect(places.size).to eq(0)
-  end
+        places = BeermappingApi.places_in("kerava")
 
-  it "When HTTP GET returns many entries, all are parsed and returned" do
-    canned_answer = <<-END_OF_STRING
-<?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id>13307</id><name>O'Connell's Irish Bar</name><status>Beer Bar</status><reviewlink>http://beermapping.com/maps/reviews/reviews.php?locid=13307</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=13307&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=13307&amp;d=1&amp;type=norm</blogmap><street>Rautatienkatu 24</street><city>Tampere</city><state></state><zip>33100</zip><country>Finland</country><phone>35832227032</phone><overall>0</overall><imagecount>0</imagecount></location><location><id>18845</id><name>Pyynikin käsityöläispanimo</name><status>Brewery</status><reviewlink>http://beermapping.com/maps/reviews/reviews.php?locid=18845</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=18845&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=18845&amp;d=1&amp;type=norm</blogmap><street>Tesoman valtatie 24</street><city>Tampere</city><state></state><zip>33300</zip><country>Finland</country><phone></phone><overall>0</overall><imagecount>0</imagecount></location><location><id>18857</id><name>Panimoravintola Plevna</name><status>Brewpub</status><reviewlink>http://beermapping.com/maps/reviews/reviews.php?locid=18857</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=18857&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=18857&amp;d=1&amp;type=norm</blogmap><street>Itäinenkatu 8</street><city>Tampere</city><state></state><zip>33210</zip><country>Finland</country><phone></phone><overall>0</overall><imagecount>0</imagecount></location></bmp_locations>
-    END_OF_STRING
+        expect(places.size).to eq(0)
+      end
 
-    stub_request(:get, /.*tampere/).to_return(body: canned_answer, headers: { 'Content-Type' => "text/xml" })
+      it "When HTTP GET returns many entries, all are parsed and returned" do
 
-    places = BeermappingApi.places_in("tampere")
+        canned_answer = <<-END_OF_STRING
+  <?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id>13307</id><name>O'Connell's Irish Bar</name><status>Beer Bar</status><reviewlink>http://beermapping.com/maps/reviews/reviews.php?locid=13307</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=13307&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=13307&amp;d=1&amp;type=norm</blogmap><street>Rautatienkatu 24</street><city>Tampere</city><state></state><zip>33100</zip><country>Finland</country><phone>35832227032</phone><overall>0</overall><imagecount>0</imagecount></location><location><id>18845</id><name>Pyynikin käsityöläispanimo</name><status>Brewery</status><reviewlink>http://beermapping.com/maps/reviews/reviews.php?locid=18845</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=18845&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=18845&amp;d=1&amp;type=norm</blogmap><street>Tesoman valtatie 24</street><city>Tampere</city><state></state><zip>33300</zip><country>Finland</country><phone></phone><overall>0</overall><imagecount>0</imagecount></location><location><id>18857</id><name>Panimoravintola Plevna</name><status>Brewpub</status><reviewlink>http://beermapping.com/maps/reviews/reviews.php?locid=18857</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=18857&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=18857&amp;d=1&amp;type=norm</blogmap><street>Itäinenkatu 8</street><city>Tampere</city><state></state><zip>33210</zip><country>Finland</country><phone></phone><overall>0</overall><imagecount>0</imagecount></location></bmp_locations>
+        END_OF_STRING
 
-    expect(places.size).to eq(3)
-    place = places.last
-    expect(place.name).to eq("Panimoravintola Plevna")
-    expect(place.street).to eq("Itäinenkatu 8")
-  end
-  end
+        stub_request(:get, /.*tampere/).to_return(body: canned_answer, headers: { 'Content-Type' => "text/xml" })
 
-  describe "in case of cache hit" do
+        places = BeermappingApi.places_in("tampere")
 
-    it "When one entry in cache, it is returned" do
-      canned_answer = <<-END_OF_STRING
-<?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id>13307</id><name>O'Connell's Irish Bar</name><status>Beer Bar</status><reviewlink>http://beermapping.com/maps/reviews/reviews.php?locid=13307</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=13307&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=13307&amp;d=1&amp;type=norm</blogmap><street>Rautatienkatu 24</street><city>Tampere</city><state></state><zip>33100</zip><country>Finland</country><phone>35832227032</phone><overall>0</overall><imagecount>0</imagecount></location></bmp_locations>
-      END_OF_STRING
-
-      stub_request(:get, /.*espoo/).to_return(body: canned_answer, headers: {'Content-Type' => "text/xml"})
-
-      BeermappingApi.places_in("espoo")
-
-      places = BeermappingApi.places_in("espoo")
-
-      expect(places.size).to eq(1)
-      place = places.first
-      expect(place.name).to eq("O'Connell's Irish Bar")
-      expect(place.street).to eq("Rautatienkatu 24")
+        expect(places.size).to eq(3)
+        place = places.first
+        expect(place.name).to eq("O'Connell's Irish Bar")
+        expect(place.street).to eq("Rautatienkatu 24")
+      end
     end
-  end
+
+    describe "in case of cache hit" do
+
+      it "When one entry in cache, it is returned" do
+        canned_answer = <<-END_OF_STRING
+<?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id>13307</id><name>O'Connell's Irish Bar</name><status>Beer Bar</status><reviewlink>http://beermapping.com/maps/reviews/reviews.php?locid=13307</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=13307&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=13307&amp;d=1&amp;type=norm</blogmap><street>Rautatienkatu 24</street><city>Tampere</city><state></state><zip>33100</zip><country>Finland</country><phone>35832227032</phone><overall>0</overall><imagecount>0</imagecount></location></bmp_locations>
+        END_OF_STRING
+
+        stub_request(:get, /.*espoo/).to_return(body: canned_answer, headers: {'Content-Type' => "text/xml"})
+
+        # ensure that data found in cache
+        BeermappingApi.places_in("espoo")
+
+        places = BeermappingApi.places_in("espoo")
+
+        expect(places.size).to eq(1)
+        place = places.first
+        expect(place.name).to eq("O'Connell's Irish Bar")
+        expect(place.street).to eq("Rautatienkatu 24")
+      end
+    end
 end
