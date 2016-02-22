14c080393d0090823a3ed8077f68a37a132a6ceb
Sara Kauvo <sara.kau@hotmail.com>
2016-02-22 17:02:50 +0200

    testit

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,55 +28,26 @@ class User < ActiveRecord::Base
     ratings.order(score: :desc).limit(1).first.beer
   end
 
-  def favorite_brewery
-    return nil if ratings.empty?
-    bre = {}
-    ratings.each do |rating|
-      bre[rating.beer.brewery] = 0
-    end
-    ratings.each do |rating|
-      bre.keys.each do |brewery|
-        if brewery == rating.beer.brewery
-          bre[brewery] = bre[brewery] + rating.score
-        end
-      end
-    end
-    iso = 0
-    tulos = ""
-    bre.each do |score|
-        if score.last > iso
-          iso = score.last
-          tulos = score.first.name
-        end
-    end
-    return tulos
+  def favorite_style
+    favorite :style
   end
 
-  def favorite_style
-    return nil if ratings.empty?
-    sty = {}
-    ratings.each do |rating|
-      x = Style.find rating.beer.style_id
-      sty[x.name] = 0
-    end
-    ratings.each do |rating|
-      sty.keys.each do |style|
-        x = Style.find rating.beer.style_id
-        if style == x.name
-          sty[style] = sty[style] + rating.score
-        end
-      end
-    end
-    iso = 0
-    tulos = ""
-    sty.each do |score|
-      if score.last > iso
-        iso = score.last
-        tulos = score.first
-      end
-    end
-    return tulos
-    end
+  def favorite_brewery
+   favorite :brewery
+  end
+
+  def favorite(category)
+   return nil if ratings.empty?
+
+   rated = ratings.map{ |r| r.beer.send(category) }.uniq
+   rated.sort_by { |item| -rating_of(category, item) }.first.name
+  end
+
+  def rating_of(category, item)
+    ratings_of = ratings.select{ |r| r.beer.send(category)==item }
+    ratings_of.map(&:score).inject(&:+) / ratings_of.count.to_f
+  end
+
 
   def self.top(n)
     sorted_by_rating_in_desc_order = User.all.sort_by{ |b| -(b.average_rating||0)}
